7f60d7154b5d58a4dbcc382215f8c97759d40341
levserj <levserj@yahoo.com>
2016-09-29 14:36:07 +0300

    - Added comments   Added set cleaning

--- src/main/java/UniqueSymbolsStringFinder.java
+++ src/main/java/UniqueSymbolsStringFinder.java
@@ -1,37 +1,57 @@
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 
 /**
  * Created by Levchynskyi Serhii on 28.09.16.
- *
- *
+ * <p/>
+ * Method findStringOfUniqueSymbols() takes as an argument input String,
+ * and returns substring of the input with only unique symbols,
+ * if there is such substring. If not returns "No such substring".
  */
 public class UniqueSymbolsStringFinder {
 
     private HashSet<Character> set = new HashSet<Character>();
     private HashSet<Character> backupSet = new HashSet<Character>();
 
-    public String findStringOfUniqueSymbols(String inputStr){
-        if (inputStr.length()==0){
+    /**
+     * This method uses HashSet's property "no duplicates" to fetch
+     * all unique symbols from the initial String. Then it goes
+     * through the string in a loop, looking on every substring with
+     * length equal to set's size, comparing their symbols with
+     * symbols in the set. To compare them it deletes each symbol
+     * from the copy of the set.
+     */
+    public String findStringOfUniqueSymbols(String inputStr) {
+        // Zero length string handle.
+        if (inputStr.length() == 0) {
             return "";
         }
-        char [] chars = inputStr.toCharArray();
-        for (char c : chars){
+        // Fetching unique symbols to set
+        char[] chars = inputStr.toCharArray();
+        for (char c : chars) {
             set.add(c);
         }
-
-        for (int i=0; i<chars.length-set.size(); i++){
+        int setSize = set.size();
+        // Loop to go through input string
+        for (int i = 0; i < chars.length - setSize; i++) {
+            // Making a copy of the set
+            backupSet.clear();
             backupSet.addAll(set);
-            for (int j= i; j<j+set.size(); j++){
-                if (!backupSet.remove(chars[j])){
+            // Loop to compare every symbol in substring with symbols in the set.
+            for (int j = i; j < j + setSize; j++) {
+                // If it's impossible to delete symbol from the set(it's already deleted),
+                // this isn't the string we are looking for
+                if (!backupSet.remove(chars[j])) {
                     break;
                 }
-                if (backupSet.size()==0){
-                    return inputStr.substring(i, i+set.size());
+                // If all the symbols are deleted - we have our winner
+                if (backupSet.size() == 0) {
+                    set.clear();
+                    return inputStr.substring(i, i + setSize);
                 }
             }
-            backupSet.clear();
         }
+        set.clear();
+        // If nothing found
         return "No such substring";
     }
 }
--- src/test/java/UniqueSymbolsStringFinderTest.java
+++ src/test/java/UniqueSymbolsStringFinderTest.java
@@ -3,7 +3,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
@@ -15,29 +14,30 @@ public class UniqueSymbolsStringFinderTest {
     private static UniqueSymbolsStringFinder usf;
 
     @BeforeClass
-    public static void init (){
+    public static void init() {
         usf = new UniqueSymbolsStringFinder();
     }
 
     @Test
-    public void givenStringResultsInCorrectOutputString(){
+    public void givenStringResultsInCorrectOutputString() {
         assertThat(usf.findStringOfUniqueSymbols("abbcdabc"), equalTo("bcda"));
         assertThat(usf.findStringOfUniqueSymbols("cbacdcbc"), equalTo("bacd"));
+        assertThat(usf.findStringOfUniqueSymbols("aaabcaaa"), equalTo("abc"));
     }
 
     @Test
-    public void stringWithoutSubStrOfUniqueSymbolsResultsAsExpected(){
+    public void stringWithoutSubStrOfUniqueSymbolsResultsAsExpected() {
         assertThat(usf.findStringOfUniqueSymbols("aabbc"), equalTo("No such substring"));
         assertThat(usf.findStringOfUniqueSymbols("abbcdabcx"), equalTo("No such substring"));
     }
 
     @Test
-    public void emptyStringProcessedCorrectly(){
+    public void emptyStringProcessedCorrectly() {
         assertThat(usf.findStringOfUniqueSymbols(""), equalTo(""));
     }
 
     @Test
-    public void stringConsistOfSameOneSymbolProcessedCorrectly(){
+    public void stringConsistOfSameOneSymbolProcessedCorrectly() {
         assertThat(usf.findStringOfUniqueSymbols("aaaaaaaaa"), equalTo("a"));
     }
 }
